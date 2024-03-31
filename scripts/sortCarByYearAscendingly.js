9739866e9205954f724dd05954a98d8060d5534b
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2024-04-01 06:07:40 +0700

    fix: add one-liners sort method

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,14 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  result.sort((a, b) => a.year - b.year);
+
+  // Rubah code ini dengan array hasil sorting secara ascending
+  return result;
+}
