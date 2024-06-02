2a5460b84d7057271d008a0cc315d478f7623215
Mhd. Zulfikar Pinem <zulfikarm022@gmail.com>
2024-06-02 23:11:24 +0700

    fix: add created_by and updated_by

--- db/seeds/20240510063425_car.seed.ts
+++ db/seeds/20240510063425_car.seed.ts
@@ -11,24 +11,32 @@ export async function seed(knex: Knex): Promise<void> {
             rent_per_day: 200000,
             size_id: 2,
             image: "/public/uploads/images/avanza.jpg",
+            created_by: 1,
+            updated_by: 1,
         },
         {
             name: "Daihatsu Xenia",
             rent_per_day: 250000,
             size_id: 2,
             image: "/public/uploads/images/xenia.jpg",
+            created_by: 1,
+            updated_by: 1,
         },
         {
             name: "Honda Jazz",
             rent_per_day: 300000,
             size_id: 1,
             image: "/public/uploads/images/jazz.png",
+            created_by: 1,
+            updated_by: 1,
         },
         {
             name: "Toyota Fortuner",
             rent_per_day: 500000,
             size_id: 3,
             image: "/public/uploads/images/fortuner.jpg",
+            created_by: 1,
+            updated_by: 1,
         },
     ])
 }
